51a05223b0d65865ed8831eaa0ae7448b1dee384
GODZISOP <appointmentstudio@gmail.com>
2026-08-14 23:31:52 +0500

    Add 4 cheer images to Cheer section

--- src/components/Cheer.tsx
+++ src/components/Cheer.tsx
@@ -1,17 +1,29 @@
 import Image from 'next/image';
 import styles from './Cheer.module.css';
 
+const cheerImages = [
+  { src: '/cheer3.png', alt: 'Cheer Image 1' },
+  { src: '/cheer4.png', alt: 'Cheer Image 2' },
+  { src: '/cheer5.png', alt: 'Cheer Image 3' },
+  { src: '/cheer6.png', alt: 'Cheer Image 4' },
+];
+
 export default function Cheer() {
   return (
     <section className={styles.section}>
       <h2 className={styles.title}>PUT A LITTLE CHEER IN YOUR LIFE!</h2>
       <div className={styles.grid}>
-        <div className={styles.imageWrapper}>
-          <Image src="/cheer1.png" alt="Cheer 1" fill className={styles.image} />
-        </div>
-        <div className={styles.imageWrapper}>
-          <Image src="/cheer2.png" alt="Cheer 2" fill className={styles.image} />
-        </div>
+        {cheerImages.map((img, idx) => (
+          <div key={idx} className={styles.imageWrapper}>
+            <Image 
+              src={img.src} 
+              alt={img.alt} 
+              fill 
+              className={styles.image} 
+              unoptimized
+            />
+          </div>
+        ))}
       </div>
     </section>
   );
